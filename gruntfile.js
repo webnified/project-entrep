dc45c74cb08c3694f5bff447231b866330d7682f
Niel Patrick M. Lugpatan <logi.ebil@outlook.com>
2014-08-31 16:53:34 +0800

    Updated gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -3,32 +3,6 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 	  pkg: grunt.file.readJSON('package.json'),
-	  
-	  // CSSJoin
-    cssjoin: {
-	    production : {
-	      options: {
-	        // Task-specific options go here.
-	      },
-	      your_target: {
-	        // Target-specific file lists and/or options go here.
-	      },
-	    }
-	  },
-
-	  datauri: {
-      default: {
-        options: {
-            classPrefix: 'data-'
-        },
-        src: [
-          // Source file here.
-        ],
-        dest: [
-          // Destination file go here.
-        ]
-      }
-    }
 
 	  // Recess
 	  recess: {
@@ -39,7 +13,7 @@ module.exports = function(grunt) {
 			  		compress: true
 			  	},
 			  	files: {
-			  		'css/responsive-styles.css': ['less/style.less']
+			  		'css/style.css': ['less/*.less']
 			  	}
 
 		  	}
